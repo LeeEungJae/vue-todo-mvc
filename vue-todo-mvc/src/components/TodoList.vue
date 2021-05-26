4ccd1dbd7b9bae5c46ff33ece278ccf3ff6bc8c4
eungjlee <eungjlee@smilegate.com>
2021-05-26 21:24:31 +0900

    refor: mapGetters 적용

--- vue-todo-mvc/src/components/TodoList.vue
+++ vue-todo-mvc/src/components/TodoList.vue
@@ -2,7 +2,7 @@
   <div>
     <transition-group name="list" tag="ul">
       <li
-        v-for="(todoItem, index) in this.$store.state.todoItems"
+        v-for="(todoItem, index) in this.storedTodoItems"
         v-bind:key="todoItem.item"
         class="shadow"
       >
@@ -25,6 +25,8 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   methods: {
     removeTodo(todoItem, index) {
@@ -34,6 +36,9 @@ export default {
       this.$store.commit("toggleOneItem", { todoItem, index });
     },
   },
+  computed: {
+    ...mapGetters(['storedTodoItems']);
+  }
 };
 </script>
 
