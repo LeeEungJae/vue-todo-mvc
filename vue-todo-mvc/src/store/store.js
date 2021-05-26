5e914062a306662818c7f35327c0c783f21eed95
eungjlee <eungjlee@smilegate.com>
2021-05-26 21:52:17 +0900

    refor: store 모듈화 방법

--- vue-todo-mvc/src/store/store.js
+++ vue-todo-mvc/src/store/store.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as getters from './getters'
+import * as mutations from './mutations'
 
 //use : Vue의 플러그인
 // vue에서 global로 사용할 것을 등록할 때 사용
@@ -23,36 +25,6 @@ export const store = new Vuex.Store({
   state: {
     todoItems: storage.fetch(),
   },
-  getters:{
-    storedTodoItems(state){
-      return state.todoItems;
-    }
-  },
-  mutations: {
-    addOneItem(state, todoItem) {
-      const obj = {
-        completed: false,
-        item: todoItem,
-      };
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      state.todoItems.push(obj);
-    },
-    removeOneItem(state, payload) {
-      localStorage.removeItem(payload.todoItem);
-      state.todoItems.splice(payload.index, 1);
-    },
-    toggleOneItem(state, payload) {
-      state.todoItems[payload.index].completed = !state.todoItems[payload.index]
-        .completed;
-      localStorage.removeItem(payload.todoItem.item);
-      localStorage.setItem(
-        payload.todoItem.item,
-        JSON.stringify(payload.todoItem)
-      );
-    },
-    clearAllItem(state) {
-      localStorage.clear();
-      state.todoItems = [];
-    },
-  },
+  getters: getters,
+  mutations: mutations
 });
